e9dd24ddd0a335f9da5dccc53ea2f24b523517ac
Marcin Daczkowski <merdacz@gmail.com>
2013-01-14 22:12:26 +0100

    Test runs for IE, Chrome and Safari

--- Source/Net.Daczkowski.AspNetTesting.FunctionalTests/CartTests.cs
+++ Source/Net.Daczkowski.AspNetTesting.FunctionalTests/CartTests.cs
@@ -2,20 +2,27 @@
 {
     using System;
     using System.Globalization;
+    using System.IO;
 
     using FluentAssertions;
 
     using NUnit.Framework;
 
     using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
     using OpenQA.Selenium.Firefox;
+    using OpenQA.Selenium.IE;
+    using OpenQA.Selenium.Safari;
 
     public class CartTests
     {
         [Test]
         public void GivenCartWithProduct_WhenItsPriceChanges_ShouldRecalculateAndNotify()
         {
-            IWebDriver driver = new FirefoxDriver();
+            // IWebDriver driver = new FirefoxDriver();
+            // IWebDriver driver = new InternetExplorerDriver(GetDriverDirectory());
+            // IWebDriver driver = new ChromeDriver(GetDriverDirectory());
+            IWebDriver driver = new SafariDriver();
             driver.Manage().Window.Maximize();
             driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
 
@@ -35,6 +42,35 @@
             driver.FindElements(By.ClassName("data-automation-message-pricechanged")).Should().HaveCount(1);
 
             driver.Quit();
+        }
+
+        private static string GetDriverDirectory()
+        {
+            var currentDir = Directory.GetCurrentDirectory();
+            var packagesDir = LocateDir(currentDir, "tools");
+
+            return new DirectoryInfo(packagesDir).FullName;
+        }
+
+        private static string LocateDir(string currentDir, string dirToFind)
+        {
+            if (currentDir.ToLower().Contains(dirToFind))
+            {
+                return currentDir;
+            }
+
+            var dirPath = new DirectoryInfo(currentDir).Parent.FullName;
+            string[] dirs = Directory.GetDirectories(dirPath);
+
+            foreach (string dir in dirs)
+            {
+                if (dir.ToLower().Contains(dirToFind))
+                {
+                    return dir;
+                }
+            }
+
+            return LocateDir(dirPath, dirToFind);
         }
     }
 }
