3f49087caeae649df77d7c291c19e9c469b49d82
Marcin Daczkowski <merdacz@gmail.com>
2013-01-14 22:22:01 +0100

    Getting rid of implicit waits

--- Source/Net.Daczkowski.AspNetTesting.FunctionalTests/CartTests.cs
+++ Source/Net.Daczkowski.AspNetTesting.FunctionalTests/CartTests.cs
@@ -13,32 +13,38 @@
     using OpenQA.Selenium.Firefox;
     using OpenQA.Selenium.IE;
     using OpenQA.Selenium.Safari;
+    using OpenQA.Selenium.Support.UI;
 
     public class CartTests
     {
         [Test]
         public void GivenCartWithProduct_WhenItsPriceChanges_ShouldRecalculateAndNotify()
         {
-            // IWebDriver driver = new FirefoxDriver();
+            IWebDriver driver = new FirefoxDriver();
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+
             // IWebDriver driver = new InternetExplorerDriver(GetDriverDirectory());
             // IWebDriver driver = new ChromeDriver(GetDriverDirectory());
-            IWebDriver driver = new SafariDriver();
+            // IWebDriver driver = new SafariDriver();
             driver.Manage().Window.Maximize();
-            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
 
             driver.Navigate().GoToUrl("http://localhost/Net.Daczkowski.AspNetTesting.Web/");
+
+            wait.Until(d => d.FindElement(By.ClassName("data-automation-buyitem")));
             driver.FindElement(By.ClassName("data-automation-buyitem")).Click();
             driver.FindElement(By.ClassName("data-automation-cartitem-name")).Text.Should().NotBeNullOrEmpty();
             driver.FindElements(By.ClassName("data-automation-message-pricechanged")).Should().BeEmpty();
             driver.FindElement(By.LinkText("Go to admin dashboard")).Click();
 
+            wait.Until(d => d.FindElement(By.Name("newPrice")));
             var priceField = driver.FindElement(By.Name("newPrice"));
             var newPrice = decimal.Parse(priceField.GetAttribute("value"), CultureInfo.InvariantCulture) + 1;
             priceField.Clear();
             priceField.SendKeys(newPrice.ToString(CultureInfo.InvariantCulture));
             driver.FindElement(By.ClassName("data-automation-changeprice")).Click();
-
             driver.FindElement(By.LinkText("Go to home page")).Click();
+
+            wait.Until(d => d.FindElement(By.ClassName("data-automation-message-pricechanged")));
             driver.FindElements(By.ClassName("data-automation-message-pricechanged")).Should().HaveCount(1);
 
             driver.Quit();
